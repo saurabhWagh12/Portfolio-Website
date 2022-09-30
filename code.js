85ae40f5b3d31e6f731021b2ba009082c21a6c70
Saurabh Wagh <111182719+saurabhWagh12@users.noreply.github.com>
2022-09-30 12:12:34 +0530

    Add files via upload
    Portfolio Website build only with html ,css and some javascript , include various functions like dark and light theme, scroll up pointer and many more.

---
+++ code.js
@@ -0,0 +1,98 @@
+//MENU SHOW HIDDEN
+const navMenu = document.querySelector("#nav-menu"),
+      navToggle = document.querySelector("#nav-toggle"),
+      navClose = document.querySelector("#nav-close");
+
+if(navToggle){
+    navToggle.addEventListener("click",function(){
+        navMenu.classList.add("show-menu");
+    });
+}
+
+// nav hidden
+if(navClose){
+    navClose.addEventListener("click",function(){
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+const navLink = document.querySelector("#nav-link");
+if(navLink){
+    navLink.addEventListener("click",linkAction);
+}
+
+function linkAction(){
+    const navMenu = document.querySelector("#nav-menu");
+    navMenu.classList.remove("show-menu");
+}
+
+//Scroll Section Active Links::::::
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+//Change Background Headers
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+//Show Scroll UP
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+// ******************Dark Light Theme*******************
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
